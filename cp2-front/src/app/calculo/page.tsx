f2870dfe6f5e76b3564fa31f3472f236da16e878
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-10-02 09:59:00 -0300

    página do cálculo criada

--- cp2-front/src/app/calculo/page.tsx
+++ cp2-front/src/app/calculo/page.tsx
@@ -2,6 +2,9 @@ export default function Calculo() {
     return (
       <div>
         <h1>Cálculo</h1>
+        <div>
+          <p>A <sup> k </sup>  =def A <sup>oo...o</sup> ( &quot;o&quot; k vezes, para k &gt; 1)</p>
+        </div>
       </div>
     )
   }
