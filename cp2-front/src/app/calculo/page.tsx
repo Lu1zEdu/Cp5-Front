c4b726c4e1a61c1dfb3a4b4e6747c98e33cd380d
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-10-02 16:28:30 -0300

    corrigindo página de cálculo

--- cp2-front/src/app/calculo/page.tsx
+++ cp2-front/src/app/calculo/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image"
+
 export default function Calculo() {
     return (
       <div>
         <h1 className="Hs">Cálculo</h1>
         <div>
-          <p className="Ps">A <sup> k </sup>  =def A <sup>oo...o</sup> ( &quot;o&quot; k vezes, para k &gt; 1)</p>
+          <p className="Ps">Immanuel Velikovsky não é conhecido por ter desenvolvido fórmulas científicas reconhecidas, pois sua abordagem aos eventos cósmicos e históricos era mais baseada em interpretação de mitologia e textos antigos do que em ciência empírica e matemática. Ele não elaborou equações formais ou fórmulas em seus trabalhos como se veria em teorias científicas rigorosas.
+
+Se você está buscando uma fórmula matemática ou física específica atribuída a Velikovsky, infelizmente não há registros de tal fórmula. Sua obra mais famosa, "Worlds in Collision", foca em especulações sobre a interação entre planetas e catástrofes, mas não inclui cálculos matemáticos detalhados ou fórmulas.
+
+A ciência mainstream rejeitou suas ideias por falta de evidências empíricas e pela ausência de um rigor matemático.</p>
         </div>
+        <div>
+        <Image className="m-12" src="/img/immanuel.jpeg"
+        width={800}
+        height={1000}
+        alt="Capa do livro Worlds in Collision"/>
+      </div>
       </div>
     )
   }
